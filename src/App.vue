b484617a3bb49120b9ff540aeae1357f6a30959b
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-11-11 16:08:36 +0100

    Add NewsletterForm component and basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Sign up for the newsletter!</h1>
+  <NewsletterForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewsletterForm from './components/NewsletterForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewsletterForm
   }
 }
 </script>
@@ -17,8 +17,6 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/NewsletterForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="form">
+        <div class="formInput">
+            <label for="name">Name </label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div class="formInput">
+            <label for="name">Surname </label>
+            <input type="text" id="surname" name="surname" required>
+        </div>
+        <div class="formInput">
+            <label for="email">Email </label>
+            <input type="text" id="email" name="email" required>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewsletterForm'
+}
+</script>
+
+<style scoped>
+.form {
+    display: flex;
+    flex-direction: column;
+}
+</style>
